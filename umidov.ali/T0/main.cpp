429d7e0ad1e851d993da325966f59af9a55212e9
Dmitriy <gdk1743@gmail.com>
2024-03-24 23:51:56 +0300

    Revert "umidov.ali/T0"

--- umidov.ali/T0/main.cpp
+++ umidov.ali/T0/main.cpp
@@ -1,17 +1,2 @@
-#include <iostream>
-#include <string>
-
-int main() {
-    char u = 'u';
-    char m = 'm';
-    char i = 'i';
-    char d = 'd';
-    char o = 'o';
-    char v = 'v';
-    char dot = '.';
-    char a = 'a';
-    char l = 'l';
-    std::string name = { u, m, i, d, o, v, dot, a, l, 'i' };
-    std::cout << name << '\n';
-    return 0;
-}
+int main()
+{}
