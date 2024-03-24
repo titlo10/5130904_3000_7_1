1f81dbc09bc512d477364dbdd48b7e676ae27f05
Алишер Умидов <152159498+UmidovAli@users.noreply.github.com>
2024-03-24 21:03:48 +0500

    Update main.cpp

--- umidov.ali/T0/main.cpp
+++ umidov.ali/T0/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <string>
 
-int main()
-{
-  std::cout << umidov.ali\n";
-  return 0;
+int main() {
+    
+    char u = 'u';
+    char m = 'm';
+    char i = 'i';
+    char d = 'd';
+    char o = 'o';
+    char v = 'v';
+    char dot = '.';
+    char a = 'a';
+    char l = 'l';
+
+    
+    std::string name = { u, m, i, d, o, v, dot, a, l, 'i' };
+
+    
+    std::cout << name << '\n';
+
+    return 0;
 }
