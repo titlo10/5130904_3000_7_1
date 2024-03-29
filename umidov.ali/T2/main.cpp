a9624065406d24cc819be94ebe3c642c7279630c
Алишер Умидов <152159498+UmidovAli@users.noreply.github.com>
2024-03-29 21:27:48 +0500

    attempt 4 for T2

--- umidov.ali/T2/main.cpp
+++ umidov.ali/T2/main.cpp
@@ -14,65 +14,44 @@ struct DataStruct {
 };
 
 std::istream& operator>>(std::istream& is, DataStruct& data) {
-    data = DataStruct();
-    char c;
-    is >> c;
+    std::string line;
+    if (std::getline(is, line)) {
+        std::istringstream iss(line);
+        char colon;
+        std::string key;
 
-    while (is >> c && c != ':') {}
-
-    std::string keyName, keyValue;
-    while (is >> keyName) {
-        if (keyName.back() != ':') {
-            is.setstate(std::ios_base::failbit);
-            return is;
-        }
-        keyName.pop_back();
-        if (keyName == "key1") {
-            if (!(is >> data.key1)) {
-                is.setstate(std::ios_base::failbit);
-                return is;
+        while (iss >> key >> colon && colon == ':') {
+            if (key == "key1") {
+                if (!(iss >> data.key1)) return is;
             }
-        }
-        else if (keyName == "key2") {
-            if (!(is >> data.key2)) {
-                is.setstate(std::ios_base::failbit);
-                return is;
+            else if (key == "key2") {
+                if (!(iss >> data.key2)) return is;
             }
-        }
-        else if (keyName == "key3") {
-            if (!(is >> std::quoted(data.key3))) {
-                is.setstate(std::ios_base::failbit);
-                return is;
+            else if (key == "key3") {
+                if (!(iss >> std::quoted(data.key3))) return is;
             }
-        }
-        else {
-            std::getline(is, keyValue, ':');
         }
     }
     return is;
 }
 
 std::ostream& operator<<(std::ostream& os, const DataStruct& data) {
-    os << "(:key1 " << data.key1 << " :key2 " << data.key2 << " :key3 " << std::quoted(data.key3) << ")";
+    os << "DataStruct { key1: " << data.key1
+        << ", key2: " << data.key2
+        << ", key3: " << std::quoted(data.key3) << " }";
     return os;
 }
 
 bool compareDataStruct(const DataStruct& a, const DataStruct& b) {
-    if (a.key1 != b.key1) {
-        return a.key1 < b.key1;
-    }
-    else if (a.key2 != b.key2) {
-        return a.key2 < b.key2;
-    }
-    else {
-        return a.key3.size() < b.key3.size();
-    }
+    if (a.key1 != b.key1) return a.key1 < b.key1;
+    if (a.key2 != b.key2) return a.key2 < b.key2;
+    return a.key3.size() < b.key3.size();
 }
 
 int main() {
-    setlocale(LC_ALL, "Russian");
     std::vector<DataStruct> dataVector;
     std::string line;
+
     while (std::getline(std::cin, line)) {
         std::istringstream iss(line);
         DataStruct data;
@@ -81,13 +60,14 @@ int main() {
         }
         else {
             std::cerr << "Ошибка чтения данных: " << line << std::endl;
-            iss.clear();
         }
     }
 
     std::sort(dataVector.begin(), dataVector.end(), compareDataStruct);
 
-    std::copy(dataVector.begin(), dataVector.end(), std::ostream_iterator<DataStruct>(std::cout, "\n"));
+    for (const auto& data : dataVector) {
+        std::cout << data << std::endl;
+    }
 
     return 0;
 }
