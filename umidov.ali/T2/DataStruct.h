490bce209a3a3231b6b463b580a73e8c33c76394
Алишер Умидов <152159498+UmidovAli@users.noreply.github.com>
2024-03-30 22:06:32 +0500

    attempt 12 for T2

--- umidov.ali/T2/DataStruct.h
+++ umidov.ali/T2/DataStruct.h
@@ -14,4 +14,4 @@ std::istream& operator>>(std::istream& is, DataStruct& data);
 std::ostream& operator<<(std::ostream& os, const DataStruct& data);
 bool compareDataStruct(const DataStruct& a, const DataStruct& b);
 
-#endiàf 
+#endif 
