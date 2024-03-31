5302b22490daf08bafa1867457e9a5a200bff15f
Алишер Умидов <152159498+UmidovAli@users.noreply.github.com>
2024-03-31 22:37:32 +0500

    attempt 46 for T2

--- umidov.ali/T2/DataStruct.h
+++ umidov.ali/T2/DataStruct.h
@@ -3,55 +3,16 @@
 
 #include <iostream>
 #include <string>
-#include <sstream>
-#include <algorithm>
 
 struct DataStruct {
-    unsigned long long key1 = 0;
-    unsigned long long key2 = 0;
+    unsigned long long key1;
+    unsigned long long key2;
     std::string key3;
 
-    DataStruct() = default;
-
-    friend std::istream& operator>>(std::istream& is, DataStruct& ds) {
-        std::string input, temp;
-        if (!std::getline(is, input, ')')) return is;
-        std::replace(input.begin(), input.end(), ':', ' ');
-        std::istringstream iss(input);
-        while (iss >> temp) {
-            try {
-                if (temp == "key1") {
-                    std::string ullStr;
-                    iss >> ullStr;
-                    ullStr.pop_back(); ullStr.pop_back(); ullStr.pop_back();
-                    ds.key1 = std::stoull(ullStr);
-                }
-                else if (temp == "key2") {
-                    std::string octStr;
-                    iss >> octStr;
-                    ds.key2 = std::stoull(octStr, nullptr, 8);
-                }
-                else if (temp == "key3") {
-                    iss >> std::ws;
-                    std::getline(iss, ds.key3, '\"');
-                    std::getline(iss, ds.key3, '\"');
-                }
-            }
-            catch (const std::exception& e) {
-                std::cerr << "Error parsing input: " << e.what() << std::endl;
-                is.setstate(std::ios::failbit);
-                break;
-            }
-        }
-        return is;
-    }
-
-    friend std::ostream& operator<<(std::ostream& os, const DataStruct& ds) {
-        os << "(:key1 " << ds.key1 << "ull:key2 0" << std::oct << ds.key2 << std::dec << ":key3 \"" << ds.key3 << "\":)";
-        return os;
-    }
+    friend std::istream& operator>>(std::istream& is, DataStruct& ds);
+    friend std::ostream& operator<<(std::ostream& os, const DataStruct& ds);
 };
 
 bool compareDataStructs(const DataStruct& a, const DataStruct& b);
 
-#endif
+#endif // DATASTRUCT_H
--- umidov.ali/T2/main.cpp
+++ umidov.ali/T2/main.cpp
@@ -1,7 +1,39 @@
 #include "DataStruct.h"
 #include <vector>
 #include <algorithm>
-#include <iostream>
+#include <sstream>
+#include <iomanip>
+
+std::istream& operator>>(std::istream& is, DataStruct& ds) {
+    std::string input, temp;
+    if (!std::getline(is, input, ')')) return is;
+    std::replace(input.begin(), input.end(), ':', ' ');
+    std::istringstream iss(input);
+    while (iss >> temp) {
+        if (temp == "key1") {
+            std::string ullStr;
+            iss >> ullStr;
+            ullStr.pop_back(); ullStr.pop_back(); ullStr.pop_back();
+            ds.key1 = std::stoull(ullStr);
+        }
+        else if (temp == "key2") {
+            std::string octStr;
+            iss >> octStr;
+            ds.key2 = std::stoull(octStr, nullptr, 8);
+        }
+        else if (temp == "key3") {
+            iss >> std::ws;
+            std::getline(iss, ds.key3, '"');
+            std::getline(iss, ds.key3, '"');
+        }
+    }
+    return is;
+}
+
+std::ostream& operator<<(std::ostream& os, const DataStruct& ds) {
+    os << "(:key1 " << ds.key1 << "ull:key2 0" << std::oct << ds.key2 << std::dec << ":key3 \"" << ds.key3 << "\":)";
+    return os;
+}
 
 bool compareDataStructs(const DataStruct& a, const DataStruct& b) {
     if (a.key1 != b.key1) return a.key1 < b.key1;
