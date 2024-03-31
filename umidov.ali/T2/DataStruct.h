360a4419dbc0b8a32962f22b99aa042408b469a4
Алишер Умидов <152159498+UmidovAli@users.noreply.github.com>
2024-04-01 00:01:15 +0500

    attempt 47 for T2

--- umidov.ali/T2/DataStruct.h
+++ umidov.ali/T2/DataStruct.h
@@ -1,6 +1,5 @@
 #ifndef DATASTRUCT_H
 #define DATASTRUCT_H
-
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -13,43 +12,47 @@ struct DataStruct {
 
     DataStruct() = default;
 
-    friend std::istream& operator>>(std::istream& is, DataStruct& ds) {
+    friend std::istream& operator>>(std::istream& is, DataStruct& ds)
+    {
         std::string input, temp;
         if (!std::getline(is, input, ')')) return is;
         std::replace(input.begin(), input.end(), ':', ' ');
         std::istringstream iss(input);
-        while (iss >> temp) {
-            try {
-                if (temp == "key1") {
+        while (iss >> temp)
+        {
+            try
+            {
+                if (temp == "key1")
+                {
                     std::string ullStr;
                     iss >> ullStr;
-                    // Remove "ull" from the end
                     ullStr.erase(ullStr.size() - 3);
-                    // Try converting to unsigned long long
-                    ds.key1 = std::stoull(ullStr, nullptr, 10); // Use base 10 explicitly
+                    ds.key1 = std::stoull(ullStr, nullptr, 10);
                 }
-                else if (temp == "key2") {
+                else if (temp == "key2")
+                {
                     std::string octStr;
                     iss >> octStr;
-                    ds.key2 = std::stoull(octStr, nullptr, 8); // Convert octal string to number
+                    ds.key2 = std::stoull(octStr, nullptr, 8);
                 }
                 else if (temp == "key3") {
-                    iss >> std::ws; // Skip whitespace
-                    std::getline(iss, ds.key3, '\"'); // Read string within quotes
+                    iss >> std::ws;
+                    std::getline(iss, ds.key3, '\"');
                     std::getline(iss, ds.key3, '\"');
                 }
             }
-            catch (const std::invalid_argument& ia) {
-                // Handle invalid argument exception
+            catch (const std::invalid_argument& ia)
+            {
                 std::cerr << "Invalid argument: " << ia.what() << '\n';
-                is.setstate(std::ios::failbit); // Set the failbit of the stream
-                break; // Exit the loop
+                is.setstate(std::ios::failbit);
+                break;
             }
         }
         return is;
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const DataStruct& ds) {
+    friend std::ostream& operator<<(std::ostream& os, const DataStruct& ds)
+    {
         os << "(:key1 " << ds.key1 << "ull:key2 0" << std::oct << ds.key2 << std::dec << ":key3 \"" << ds.key3 << "\":)";
         return os;
     }
@@ -57,4 +60,4 @@ struct DataStruct {
 
 bool compareDataStructs(const DataStruct& a, const DataStruct& b);
 
-#endif // DATASTRUCT_H
+#endif
