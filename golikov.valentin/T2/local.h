dc2a8b9bd44763a57b54b260507d4479ae1c1997
ValentinGolikov <triderm300@gmail.com>
2024-03-31 20:51:46 +0300

    Add ignoring of incorrect exp and different key order

---
+++ golikov.valentin/T2/local.h
@@ -0,0 +1,18 @@
+#ifndef LOCAL_H
+#define LOCAL_H
+
+//В этом файле подключаются все используемые библиотеки
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cassert>
+#include <iterator>
+#include <vector>
+#include <iomanip>
+#include <complex>
+#include <algorithm>
+#include <cmath>
+#include <exception>
+
+#endif
