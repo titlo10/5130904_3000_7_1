17127cb863dbcafbc95a18ee35494f65a89d9a37
Алишер Умидов <152159498+UmidovAli@users.noreply.github.com>
2024-03-31 19:20:43 +0500

    attempt 36 for T2

--- umidov.ali/T2/main.cpp
+++ umidov.ali/T2/main.cpp
@@ -1,36 +1,19 @@
 #include "DataStruct.h"
-#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <iterator>
 
 int main() {
     std::vector<DataStruct> dataVector;
-    std::cout << "Start reading data...\n";
 
-    // Чтение данных в вектор
-    DataStruct temp;
-    while (std::cin >> temp) {
-        dataVector.push_back(temp);
-    }
+    std::copy(std::istream_iterator<DataStruct>(std::cin),
+        std::istream_iterator<DataStruct>(),
+        std::back_inserter(dataVector));
 
-    // Проверка наличия считанных данных
-    if (dataVector.empty()) {
-        std::cout << "No data read. Exiting.\n";
-        return 0;
-    }
-    else {
-        std::cout << "Data reading completed.\nSorting...\n";
-    }
+    std::sort(dataVector.begin(), dataVector.end());
 
-    // Сортировка данных
-    std::sort(dataVector.begin(), dataVector.end(), compareDataStructs);
-
-    // Вывод отсортированных данных
-    std::cout << "Sorted data:\n";
-    for (const auto& item : dataVector) {
-        std::cout << item << "\n";
-    }
+    std::copy(dataVector.begin(), dataVector.end(),
+        std::ostream_iterator<DataStruct>(std::cout, "\n"));
 
     return 0;
 }
