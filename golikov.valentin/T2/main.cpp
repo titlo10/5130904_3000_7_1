2c2cfc7a98e5552ed7521dbf30e49cc523f99f74
ValentinGolikov <triderm300@gmail.com>
2024-03-31 21:58:39 +0300

    Add console input

--- golikov.valentin/T2/main.cpp
+++ golikov.valentin/T2/main.cpp
@@ -1,9 +1,6 @@
 #include "namespace.h"
 
-const std::string EXP1 = "(:key2 #c(1.0 1.0):key3 \"Data\":key1 0xFFFF:)";
-const std::string EXP2 = "(:key1 0xFFFF:key2 #c(1.0 5.0):key3 \"Data2\":)";
-const std::string EXP3 = "(:key3 \"Data33\":key2 #c(-1.0 2.0):key1 0xFFFA:)";
-const std::string EXP4 = "(:key1 0xAFFF:key2 #c(1.0 3.0):key3 \"Data444\":)";
+//const std::string EXP1 = "(:key2 #c(1.0 1.0):key3 \"Data\":key1 0xFFFF:)";
 
 using golikov::DataStruct;
 
@@ -11,23 +8,24 @@ int main()
 {
   try
   {
+    std::string input;
+    while (true)
+    {
+      std::string buf;
+
+      std::getline(std::cin, buf, '\n');
+
+      if (buf == "" || std::cin.eof())
+        break;
+
+      input += buf + '\n';
+    }
+
     std::vector< DataStruct > data;
-    std::istringstream iss1(EXP1);
-    std::istringstream iss2(EXP2);
-    std::istringstream iss3(EXP3);
-    std::istringstream iss4(EXP4);
+    std::istringstream iss(input);
 
     //Запись в вектор (iss1)
-    insertData(iss1, data);
-
-    //Запись в вектор (iss2)
-    insertData(iss2, data);
-
-    //Запись в вектор (iss3)
-    insertData(iss3, data);
-
-    //Запись в вектор (iss4)
-    insertData(iss4, data);
+    insertData(iss, data);
 
     //Сортировка вектора
     std::sort(std::begin(data), std::end(data), golikov::compareDataStruct);
