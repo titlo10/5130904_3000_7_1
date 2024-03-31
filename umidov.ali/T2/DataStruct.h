f5138a3bfbe77ed94e6a261452d59859e682e747
Алишер Умидов <152159498+UmidovAli@users.noreply.github.com>
2024-03-31 13:54:31 +0500

    attempt 25 for T2

--- umidov.ali/T2/DataStruct.h
+++ umidov.ali/T2/DataStruct.h
@@ -1,17 +1,14 @@
-	#ifndef DATASTRUCT_H
-	#define DATASTRUCT_H
-	#include <iostream>
-	#include <string>
-
-	struct DataStruct
-	{
-	  unsigned long long key1{};
-	  unsigned long long key2{};
-	  std::string key3;
-	};
-
-	  std::istream& operator>>(std::istream& is, DataStruct& data);
-	  std::ostream& operator<<(std::ostream& os, const DataStruct& data);
-	  bool compareDataStructs(const DataStruct& a, const DataStruct& b);
-
-	#endif
+#ifndef DATASTRUCT_H
+#define DATASTRUCT_H
+#include <iostream>
+#include <string>
+struct DataStruct
+{
+  unsigned long long key1{};
+  unsigned long long key2{};
+  std::string key3;
+};
+  std::istream& operator>>(std::istream& is, DataStruct& data);
+  std::ostream& operator<<(std::ostream& os, const DataStruct& data);
+  bool compareDataStructs(const DataStruct& a, const DataStruct& b);
+#endif
