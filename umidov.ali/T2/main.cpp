0298060da22d96b8f012438c431ec3f8f8d6fac4
Алишер Умидов <152159498+UmidovAli@users.noreply.github.com>
2024-03-31 21:10:20 +0500

    attempt 40 for T2

--- umidov.ali/T2/main.cpp
+++ umidov.ali/T2/main.cpp
@@ -1,37 +1,61 @@
 #include "DataStruct.h"
-#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <sstream>
+#include <iterator>
+#include <algorithm>
 
+bool compareDataStruct(const DataStruct& a, const DataStruct& b) {
+    if (a.key1 != b.key1) return a.key1 < b.key1;
+    if (a.key2 != b.key2) return a.key2 < b.key2;
+    return a.key3.length() < b.key3.length();
+}
+
+std::istream& operator>>(std::istream& is, DataStruct& ds)
+{
+    std::string input, token;
+    if (!std::getline(is, input, ')')) return is;
+    std::replace(input.begin(), input.end(), ':', ' ');
+    std::istringstream iss(input);
+    while (iss >> token)
+    {
+        if (token == "key1")
+        {
+            iss >> ds.key1;
+            iss.ignore(3);
+        }
+        else if (token == "key2")
+        {
+            std::string octStr;
+            iss >> octStr;
+            ds.key2 = std::stoull(octStr, nullptr, 8);
+        }
+        else if (token == "key3")
+        {
+            iss.ignore();
+            std::getline(iss, ds.key3, '"');
+        }
+    }
+    return is;
+}
+
+std::ostream& operator<<(std::ostream& os, const DataStruct& ds)
+{
+    os << "(:key1 " << ds.key1 << "ull:key2 0" << std::oct << ds.key2 << ":key3 "" << ds.key3 << "":)";
+    return os;
+}
 
 int main()
 {
-    std::vector<DataStruct> dataVector;
-    std::cout << "Start reading data...\n";
+    std::vector<DataStruct> data;
 
-    DataStruct data;
-    while (std::cin >> data)
-    {
-        dataVector.push_back(data);
-    }
+    std::copy(std::istream_iterator<DataStruct>(std::cin),
+        std::istream_iterator<DataStruct>(),
+        std::back_inserter(data));
 
-    if (dataVector.empty())
-    {
-        std::cerr << "Looks like there are no supported records. Cannot determine input. Test skipped." << std::endl;
-        return 0;
-    }
+    std::sort(data.begin(), data.end(), compareDataStruct);
 
-    std::cout << "Data reading completed." << std::endl;
-    std::cout << "Sorting..." << std::endl;
-
-    std::sort(dataVector.begin(), dataVector.end(), compareDataStructs);
-
-    std::cout << "Sorted data:" << std::endl;
-
-    for (const auto& d : dataVector)
-    {
-        std::cout << d << std::endl;
-    }
+    std::copy(data.begin(), data.end(), std::ostream_iterator<DataStruct>(std::cout, "\n"));
 
     return 0;
 }
