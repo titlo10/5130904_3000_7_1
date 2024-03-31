082ba45337b01ff87702ed5f2217a4547cf0e784
Алишер Умидов <152159498+UmidovAli@users.noreply.github.com>
2024-03-31 13:32:02 +0500

    attempt 20 for T2

--- umidov.ali/T2/DataStruct.h
+++ umidov.ali/T2/DataStruct.h
@@ -11,8 +11,8 @@ struct DataStruct
   std::string key3;
 };
 
-std::istream& operator>>(std::istream& is, DataStruct& data);
-std::ostream& operator<<(std::ostream& os, const DataStruct& data);
-bool compareDataStructs(const DataStruct& a, const DataStruct& b);
+  std::istream& operator>>(std::istream& is, DataStruct& data);
+  std::ostream& operator<<(std::ostream& os, const DataStruct& data);
+  bool compareDataStructs(const DataStruct& a, const DataStruct& b);
 
 #endif
