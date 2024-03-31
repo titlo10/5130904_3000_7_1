701dadcd7ae61606a4a10eb1a24153c8b709a2e9
Алишер Умидов <152159498+UmidovAli@users.noreply.github.com>
2024-03-31 16:06:18 +0500

    attempt 27 for T2

--- umidov.ali/T2/DataStruct.h
+++ umidov.ali/T2/DataStruct.h
@@ -4,30 +4,36 @@
 #include <sstream>
 #include <algorithm>
 
-struct DataStruct {
+struct DataStruct
+{
     int key1;
     int key2;
     std::string key3;
 };
 
-inline std::istream& operator>>(std::istream& is, DataStruct& data) {
+inline std::istream& operator>>(std::istream& is, DataStruct& data)
+{
     std::string line;
-    if (std::getline(is, line, ')')) {
+    if (std::getline(is, line, ')'))
+    {
         std::replace(line.begin(), line.end(), ':', ' ');
         std::istringstream iss(line.substr(1));
-        if (!(iss >> data.key1 >> data.key2 >> std::ws && std::getline(iss, data.key3, '\"') && std::getline(iss, data.key3, '\"'))) {
+        if (!(iss >> data.key1 >> data.key2 >> std::ws && std::getline(iss, data.key3, '\"') && std::getline(iss, data.key3, '\"')))
+        {
             is.setstate(std::ios::failbit);
         }
     }
     return is;
 }
 
-inline std::ostream& operator<<(std::ostream& os, const DataStruct& data) {
+inline std::ostream& operator<<(std::ostream& os, const DataStruct& data)
+{
     os << "(:key1 " << data.key1 << " :key2 " << data.key2 << " :key3 \"" << data.key3 << "\":)";
     return os;
 }
 
-inline bool compareDataStructs(const DataStruct& a, const DataStruct& b) {
+inline bool compareDataStructs(const DataStruct& a, const DataStruct& b)
+{
     if (a.key1 != b.key1) return a.key1 < b.key1;
     if (a.key2 != b.key2) return a.key2 < b.key2;
     return a.key3 < b.key3;
