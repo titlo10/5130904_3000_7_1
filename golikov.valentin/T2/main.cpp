17d3028ff42f56f224d5039e349c9a9b4ab5e77a
ValentinGolikov <triderm300@gmail.com>
2024-03-31 21:08:57 +0300

    Remove errors in try-catch module

--- golikov.valentin/T2/main.cpp
+++ golikov.valentin/T2/main.cpp
@@ -41,7 +41,7 @@ int main()
       std::ostream_iterator< DataStruct >(std::cout, "\n")
     );
   }
-  catch (std::exception ex)
+  catch (std::exception& const ex)
   {
     std::cerr << ex.what();
     return EXIT_FAILURE;
