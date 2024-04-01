1688a4648710747931baff1e68d0a42290911c5b
Алишер Умидов <152159498+UmidovAli@users.noreply.github.com>
2024-04-01 15:22:06 +0500

    attempt 71 for T2

--- umidov.ali/T2/main.cpp
+++ umidov.ali/T2/main.cpp
@@ -5,7 +5,6 @@
 int main() {
     std::vector<DataStruct> dataVector;
     DataStruct temp;
-    std::cout << "Enter data, finish with EOF (Ctrl+D or Ctrl+Z on UNIX, Ctrl+Z then Enter on Windows):\n";
     while (std::cin >> temp) {
         dataVector.push_back(temp);
     }
