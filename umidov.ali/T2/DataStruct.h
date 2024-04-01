37e9fab25029e662d86546f754edbc5a1037a081
Алишер Умидов <152159498+UmidovAli@users.noreply.github.com>
2024-04-01 13:45:09 +0500

    attempt 65 for T2

--- umidov.ali/T2/DataStruct.h
+++ umidov.ali/T2/DataStruct.h
@@ -1,13 +1,20 @@
+#ifndef DATASTRUCT_H
+#define DATASTRUCT_H
+
 #include <iostream>
 #include <string>
 
-struct DataStruct{
-    unsigned long long key1;
-    unsigned long long key2;
-    std::string key3;
+struct DataStruct {
+	unsigned long long key1;
+	unsigned long long key2;
+	std::string key3;
 
-    DataStruct() : key1(0), key2(0), key3("") {}
+	DataStruct() : key1(0), key2(0), key3("") {}
 
-    friend std::istream& operator>>(std::istream& is, DataStruct& ds);
-    friend std::ostream& operator<<(std::ostream& os, const DataStruct& ds);
+	friend std::istream& operator>>(std::istream& is, DataStruct& ds);
+	friend std::ostream& operator<<(std::ostream& os, const DataStruct& ds);
 };
+
+bool compareDataStructs(const DataStruct& a, const DataStruct& b);
+
+#endif
--- umidov.ali/T2/main.cpp
+++ umidov.ali/T2/main.cpp
@@ -1,49 +1,37 @@
 #include "DataStruct.h"
 #include <vector>
-#include <iostream>
 #include <algorithm>
 #include <sstream>
 #include <iomanip>
 
 std::istream& operator>>(std::istream& is, DataStruct& ds) {
-    std::string input;
-    if (!std::getline(is, input, ')'))
-    {
-        return is;
-    }
-
-    input.erase(std::remove(input.begin(), input.end(), '('), input.end());
+    std::string input, temp;
+    if (!std::getline(is, input, ')')) return is;
     std::replace(input.begin(), input.end(), ':', ' ');
-
     std::istringstream iss(input);
-    std::string temp;
-
     while (iss >> temp) {
-        if (temp == "key1")
-        {
+        if (temp == "key1") {
             std::string ullStr;
             iss >> ullStr;
-            ds.key1 = std::stoull(ullStr.substr(0, ullStr.length() - 3));
+            ullStr.pop_back(); ullStr.pop_back(); ullStr.pop_back();
+            ds.key1 = std::stoull(ullStr);
         }
-        else if (temp == "key2")
-        {
+        else if (temp == "key2") {
             std::string octStr;
             iss >> octStr;
             ds.key2 = std::stoull(octStr, nullptr, 8);
         }
         else if (temp == "key3") {
             iss >> std::ws;
-            std::getline(iss, ds.key3, '\"');
-            std::getline(iss, ds.key3, '\"');
+            std::getline(iss, ds.key3, '"');
+            std::getline(iss, ds.key3, '"');
         }
     }
     return is;
 }
 
 std::ostream& operator<<(std::ostream& os, const DataStruct& ds) {
-    os << "(:key1 " << ds.key1 << "ull:key2 0"
-        << std::oct << ds.key2 << std::dec
-        << ":key3 \"" << ds.key3 << "\":)";
+    os << "(:key1 " << ds.key1 << "ull:key2 0" << std::oct << ds.key2 << std::dec << ":key3 \"" << ds.key3 << "\":)";
     return os;
 }
 
@@ -53,20 +41,19 @@ bool compareDataStructs(const DataStruct& a, const DataStruct& b) {
     return a.key3.length() < b.key3.length();
 }
 
-int main(){
+int main() {
     std::vector<DataStruct> dataVector;
     DataStruct temp;
 
     std::cout << "Enter data, finish with EOF (Ctrl+D or Ctrl+Z):\n";
 
-    while (std::cin >> temp)
-    {
+    while (std::cin >> temp) {
         dataVector.push_back(temp);
     }
+
     std::sort(dataVector.begin(), dataVector.end(), compareDataStructs);
 
-    for (const auto& data : dataVector)
-    {
+    for (const auto& data : dataVector) {
         std::cout << data << "\n";
     }
 
