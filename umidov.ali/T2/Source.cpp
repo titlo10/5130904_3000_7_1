e5dca4a331a8572b849946f02e03d413a52309bb
Алишер Умидов <152159498+UmidovAli@users.noreply.github.com>
2024-04-01 17:08:16 +0500

    attempt 73 for T2

--- umidov.ali/T2/Source.cpp
+++ umidov.ali/T2/Source.cpp
@@ -6,32 +6,45 @@
 #include <iostream>
 #include <vector>
 std::istream& operator>>(std::istream& is, DataStruct& ds) {
-    std::string input, part;
-    if (!std::getline(is, input, ')')) return is;
-    std::replace(input.begin(), input.end(), ':', ' ');
-    input = input.substr(2, input.size() - 3);
-    std::istringstream iss(input);
-    while (iss >> part) {
-        if (part == "key1") {
-            iss >> ds.key1;
-            iss.ignore(3);
+    std::string input;
+    while (std::getline(is, input)) {
+        std::replace(input.begin(), input.end(), ':', ' ');
+        if (input.size() < 3 || input[0] != '(' || input[1] != ':') {
+            continue;
         }
-        else if (part == "key2") {
-            iss >> std::oct >> ds.key2 >> std::dec;
+        input = input.substr(2, input.size() - 3);
+        std::istringstream iss(input);
+        std::string part;
+        bool key1Found = false, key2Found = false, key3Found = false;
+        while (iss >> part) {
+            if (part == "key1" && !key1Found) {
+                if (!(iss >> ds.key1)) break;
+                iss.ignore(3);
+                key1Found = true;
+            }
+            else if (part == "key2" && !key2Found) {
+                if (!(iss >> std::oct >> ds.key2 >> std::dec)) break;
+                key2Found = true;
+            }
+            else if (part == "key3" && !key3Found) {
+                iss.ignore(1, '\"');
+                if (!std::getline(iss, ds.key3, '\"')) break;
+                key3Found = true;
+            }
         }
-        else if (part == "key3") {
-            iss.ignore(1, '\"');
-            std::getline(iss, ds.key3, '\"');
+        if (key1Found && key2Found && key3Found) {
+            return is;
         }
     }
+    is.setstate(std::ios::eofbit);
     return is;
 }
-std::ostream& operator<<(std::ostream& os, const DataStruct& ds) {
-    os << "(:key1 " << ds.key1 << "ull:key2 0" << std::oct << ds.key2 << std::dec << ":key3 \"" << ds.key3 << "\":)";
+std::ostream& operator<<(std::ostream& os, const DataStruct& ds)
+{
+    os << "(:key1 " << ds.key1 << " :key2 0" << std::oct << ds.key2 << std::dec << " :key3 \"" << ds.key3 << "\")";
     return os;
 }
-bool compareDataStructs(const DataStruct& a, const DataStruct& b) {
-    if (a.key1 != b.key1) return a.key1 < b.key1;
-    if (a.key2 != b.key2) return a.key2 < b.key2;
-    return a.key3 < b.key3;
+bool compareDataStructs(const DataStruct& a, const DataStruct& b)
+{
+    return false;
 }
