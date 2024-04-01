bba7232a795d314ff0b22e2ac9eb88755a5424e5
Алишер Умидов <152159498+UmidovAli@users.noreply.github.com>
2024-04-01 22:53:42 +0500

    attempt 73 for T2

--- umidov.ali/T2/DataStruct.h
+++ umidov.ali/T2/DataStruct.h
@@ -5,12 +5,12 @@
 #include <utility>
 #include <complex>
 namespace umidov{
-    struct DataStruct {
-        double key1 = 0.0;
-        unsigned long long key2 = 0;
-        std::string key3 = "";
+    struct DataStruct
+    {
+        unsigned long long key1;
+        unsigned long long key2;
+        std::string key3;
     };
-
     std::istream& operator>>(std::istream& in, DataStruct& dest);
     std::ostream& operator<<(std::ostream& out, const DataStruct& src);
 }
