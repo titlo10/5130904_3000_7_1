f4500e788ca8b7a2e11c1c9fe2c739c75b226eb7
Vova Anisimov <vova.anisimov38@bk.ru>
2024-04-01 14:41:01 +0300

    newdate

--- anisimov.vladimir/T2/sort.h
+++ anisimov.vladimir/T2/sort.h
@@ -9,12 +9,13 @@ struct DataStruct {
   std::complex<double> key2;
   std::string key3;
 };
+
 struct SortComparator {
   bool operator()(const DataStruct& lhs, const DataStruct& rhs) const {
     if (lhs.key1 != rhs.key1)
       return lhs.key1 < rhs.key1;
     if (lhs.key2 != rhs.key2)
       return std::abs(lhs.key2) < std::abs(rhs.key2);
-      return lhs.key3.length() < rhs.key3.length();
+    return lhs.key3.length() < rhs.key3.length();
   }
 };
