98f20749604b5ff70e2a9a5d8b94089dc805fdc9
Алишер Умидов <152159498+UmidovAli@users.noreply.github.com>
2024-04-02 01:56:42 +0500

    attempt 81 for T2

--- umidov.ali/T2/Source.cpp
+++ umidov.ali/T2/Source.cpp
@@ -43,8 +43,7 @@ std::istream& umidov::operator>>(std::istream& in, umidov::DataStruct& dest)
     }
     return in;
 }
-std::ostream& umidov::operator<<(std::ostream& out, const umidov::DataStruct& src)
-{
+std::ostream& umidov::operator<<(std::ostream& out, const umidov::DataStruct& src) {
     out << "(:key1 " << src.key1
         << ":key2 " << std::oct << src.key2 << std::dec
         << ":key3 \"" << src.key3 << "\":)";
