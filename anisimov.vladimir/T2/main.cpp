a63130477f2a9cb5522d9fab3e94f0dffb8fc378
Vova Anisimov <vova.anisimov38@bk.ru>
2024-04-01 14:58:08 +0300

    one test error

--- anisimov.vladimir/T2/main.cpp
+++ anisimov.vladimir/T2/main.cpp
@@ -5,22 +5,6 @@
 #include <string>
 #include <iomanip>
 
-struct DataStruct {
-  unsigned long long key1;
-  std::complex<double> key2;
-  std::string key3;
-};
-
-struct SortComparator {
-  bool operator()(const DataStruct& lhs, const DataStruct& rhs) const {
-    if (lhs.key1 != rhs.key1)
-      return lhs.key1 < rhs.key1;
-    if (lhs.key2 != rhs.key2)
-      return std::abs(lhs.key2) < std::abs(rhs.key2);
-    return lhs.key3.length() < rhs.key3.length();
-  }
-};
-
 int main() {
   std::vector<DataStruct> dataVector = {
     {10ull, 'c', "Data"},
