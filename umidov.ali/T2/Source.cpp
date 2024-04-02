4e67b8e1f93d5ea6c0fa9d720393a0010820d7a9
Алишер Умидов <152159498+UmidovAli@users.noreply.github.com>
2024-04-02 16:54:11 +0500

    attempt 94 for T2

--- umidov.ali/T2/Source.cpp
+++ umidov.ali/T2/Source.cpp
@@ -3,36 +3,43 @@
 #include <string>
 #include <sstream>
 #include <iomanip>
-namespace umidov
-{
-    std::istream& operator>>(std::istream& in, DataStruct& dest)
-    {
-        std::string part;
-        while (std::getline(in, part, ';'))
-        {
-            if (part.find("key1") != std::string::npos)
-            {
-                std::string number = part.substr(part.find("=") + 1);
-                dest.key1 = stod(number);
-            }
-            else if (part.find("key2") != std::string::npos)
-            {
-                std::string number = part.substr(part.find("=") + 1);
-                unsigned long long octValue = std::stoull(number, nullptr, 8);
-                dest.key2 = static_cast<char>(octValue & 0xFF);
-            }
-            else if (part.find("key3") != std::string::npos)
-            {
-                dest.key3 = part.substr(part.find("=") + 1);
-            }
-        }
-        return in;
-    }
-    std::ostream& operator<<(std::ostream& out, const DataStruct& dest)
-    {
+#include <charconv>
+namespace umidov {
+    std::ostream& operator<<(std::ostream& out, const DataStruct& dest) {
         out << "key1=" << dest.key1 << "; ";
         out << "key2=" << static_cast<int>(dest.key2) << "; ";
         out << "key3=" << dest.key3;
         return out;
+    }
+    std::istream& operator>>(std::istream& in, DataStruct& dest) {
+        std::string part;
+        while (std::getline(in, part, ';')) {
+            if (part.find("key1") != std::string::npos) {
+                std::string number = part.substr(part.find("=") + 1);
+                try {
+                    dest.key1 = std::stod(number);
+                }
+                catch (const std::invalid_argument& e) {
+                    std::cerr << "Ошибка преобразования key1: " << e.what() << '\n';
+                }
+                catch (const std::out_of_range& e) {
+                    std::cerr << "Ошибка преобразования key1: " << e.what() << '\n';
+                }
+            }
+            else if (part.find("key2") != std::string::npos) {
+                std::string value = part.substr(part.find("=") + 1);
+                if (value.front() == '\'') {
+                    dest.key2 = value.at(1);
+                }
+                else if (value.front() == '0' && (value.at(1) == 'x' || value.at(1) == 'X')) {
+                    unsigned long long hexValue = std::stoull(value, nullptr, 16);
+                    dest.key2 = static_cast<char>(hexValue & 0xFF);
+                }
+            }
+            else if (part.find("key3") != std::string::npos) {
+                dest.key3 = part.substr(part.find("=") + 1);
+            }
+        }
+        return in;
     }
 }
--- umidov.ali/T2/header.h
+++ umidov.ali/T2/header.h
@@ -1,13 +1,14 @@
 #ifndef HEADER_H
 #define HEADER_H
 #include <string>
+#include <iostream>
 namespace umidov {
     struct DataStruct {
-        double key1 = 0.0;
-        char key2 = '\0';
+        double key1;
+        char key2;
         std::string key3;
-        friend std::istream& operator>>(std::istream& in, DataStruct& dest);
-        friend std::ostream& operator<<(std::ostream& out, const DataStruct& dest);
     };
+    std::istream& operator>>(std::istream& in, DataStruct& dest);
+    std::ostream& operator<<(std::ostream& out, const DataStruct& dest);
 }
 #endif
