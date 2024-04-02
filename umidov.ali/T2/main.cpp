a96e7bd664e93bc6e8e93da14a0dfe8a24a62ca7
Алишер Умидов <152159498+UmidovAli@users.noreply.github.com>
2024-04-02 15:12:51 +0500

    attempt 90 for T2

--- umidov.ali/T2/main.cpp
+++ umidov.ali/T2/main.cpp
@@ -4,9 +4,7 @@ using namespace umidov;
 int main() {
     setlocale(LC_ALL, "rus");
     DataStruct data;
-    std::cout << "Введите данные в формате key1=<значение>;key2=<значение>;key3=<строка>:" << std::endl;
     std::cin >> data;
-    std::cout << "Вы ввели:" << std::endl;
-    std::cout << data << std::endl;
+    std::cout << data;
     return 0;
 }
