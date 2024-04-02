ffd42555a0e4adc1eb5cd788e2527d52cc017dce
Алишер Умидов <152159498+UmidovAli@users.noreply.github.com>
2024-04-02 16:00:24 +0500

    attempt 93 for T2

--- umidov.ali/T2/header.h
+++ umidov.ali/T2/header.h
@@ -1,17 +1,13 @@
 #ifndef HEADER_H
 #define HEADER_H
-#include <iostream>
-#include <vector>
-#include <sstream>
-namespace umidov
-{
-    struct DataStruct
-    {
-        double key1;
-        char key2 ;
+#include <string>
+namespace umidov {
+    struct DataStruct {
+        double key1 = 0.0;
+        char key2 = '\0';
         std::string key3;
+        friend std::istream& operator>>(std::istream& in, DataStruct& dest);
+        friend std::ostream& operator<<(std::ostream& out, const DataStruct& dest);
     };
-    std::istream& operator>>(std::istream& in, DataStruct& dest);
-    std::ostream& operator<<(std::ostream& out, const DataStruct& dest);
 }
 #endif
