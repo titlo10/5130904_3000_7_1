72a54adc0d7003ee11d95688e2dea2dad616d1ac
Алишер Умидов <152159498+UmidovAli@users.noreply.github.com>
2024-04-02 14:35:35 +0500

    attempt 88 for T2

--- umidov.ali/T2/main.cpp
+++ umidov.ali/T2/main.cpp
@@ -1,19 +1,12 @@
 #include "header.h"
 #include <iostream>
-#include "header.h"
-#include <iostream>
 using namespace umidov;
 int main() {
-    umidov::DataStruct data;
     setlocale(LC_ALL, "rus");
-    auto initialKey1 = data.key1;
-    std::cout << "Начальное значение key1: " << initialKey1 << std::endl;
-    std::cout << "Введите данные в формате key1=<double>;key2=<oct>;key3=<string>:" << std::endl;
+    DataStruct data;
+    std::cout << "Введите данные в формате key1=<значение>;key2=<значение>;key3=<строка>:" << std::endl;
     std::cin >> data;
-    auto str = data.key3;
-    for (auto c : str) {
-        std::cout << c << " ";
-    }
-    std::cout << std::endl;
+    std::cout << "Вы ввели:" << std::endl;
+    std::cout << data << std::endl;
     return 0;
 }
