7c0bbc30ba4a06a2bf5dce63458ad87dfdfcc95e
IlonaDeleske <ilona.deleske@yandex.ru>
2024-04-05 00:46:08 +0300

    Initial commit for T2

---
+++ deleske.ilona/T2/DataStruct.h
@@ -0,0 +1,55 @@
+#ifndef _DATASTRUCT_H_
+#define _DATASTRUCT_H_
+#include <iostream>
+#include <vector>
+#include <complex>
+using namespace std;
+
+struct DataStruct
+{
+    unsigned long long key1;
+    complex<double> key2;
+    string key3;
+};
+
+struct UllIO
+{
+    unsigned long long &ref;
+};
+
+struct ComplexIO
+{
+    complex<double> &ref;
+};
+
+struct StringIO
+{
+    string &ref;
+};
+
+struct DelimiterIO
+{
+    string delim;
+};
+
+class ResPars
+{
+    public:
+        ResPars(basic_ios<char> &strm);
+        ~ResPars();
+    private:
+        basic_ios<char> &strm_;
+        char fill_;
+        streamsize precision_;
+        basic_ios<char>::fmtflags fmtFlags_;
+};
+
+istream& operator>>(istream &in, DelimiterIO&& elem);
+istream& operator>>(istream &in, UllIO &elem);
+istream& operator>>(istream &in, ComplexIO &elem);
+istream& operator>>(istream &in, StringIO &elem);
+istream& operator>>(istream &in, DataStruct &elem);
+ostream& operator<<(ostream &op, const DataStruct &elem);
+#endif
+
+
---
+++ deleske.ilona/T2/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <iterator>
+#include "DataStruct.h"
+using namespace std;
+int main()
+{
+    vector<DataStruct> ds_original;
+    string request;
+    while (getline(cin, request))
+    {
+        istringstream iss(request);
+        DataStruct temp;
+        if (iss >> temp)
+        {
+            ds_original.push_back(temp);
+        }
+    }
+    sort(ds_original.begin(), ds_original.end(),
+    [](const DataStruct &ds1, const DataStruct &ds2)
+    {
+        if (ds1.key1 != ds2.key1)
+            return ds1.key1 < ds2.key1;
+        
+        if (abs(ds1.key2) != abs(ds2.key2))
+            return abs(ds1.key2) < abs(ds2.key2);
+        
+        return ds1.key3.length() < ds2.key3.length();
+    }
+    );
+    copy(ds_original.begin(), ds_original.end(), ostream_iterator<DataStruct>(cout, "\n"));
+    return 0;
+}
+
+
+
