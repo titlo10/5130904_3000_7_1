2a206b09a7bf413db14ce025f9c0b2afef059225
Алишер Умидов <152159498+UmidovAli@users.noreply.github.com>
2024-04-05 15:29:28 +0500

    106 for t2

--- umidov.ali/T2/Source.cpp
+++ umidov.ali/T2/Source.cpp
@@ -1,28 +1,140 @@
 #include "item_data.h"
-using umidov::DataStruct;
-int main()
-{
-    try
-    {
-        std::string userInput = "";
-        std::vector<DataStruct> dataCollection;
-        while (std::getline(std::cin, userInput))
-        {
-            std::istringstream inputStream(userInput);
-            DataStruct temporaryData;
-            if (inputStream >> temporaryData)
-            {
-                dataCollection.push_back(temporaryData);
-            }
-        }
-        std::sort(std::begin(dataCollection), std::end(dataCollection), umidov::compareDataStruct);
 
-        std::copy(std::begin(dataCollection), std::end(dataCollection), std::ostream_iterator<DataStruct>(std::cout, "\n"));
+namespace umidov {
+    std::string binaryNull(unsigned long long ref) {
+        std::stringstream ss;
+        ss << "0b" << std::setfill('0') << std::setw(2) << std::to_string(ref);
+        std::string out = ss.str();
+        size_t i = out.find('b');
+        if (out[i + 1] == '0' and out[i + 2] == '0') {
+            out.erase(i + 2, 1);
+        }
+        return out;
     }
-    catch (std::exception& ex)
-    {
-        std::cerr << ex.what();
-        return EXIT_FAILURE;
+
+    std::istream& operator>>(std::istream& in, DelimiterIO&& dest) {
+        std::istream::sentry sentry(in);
+        if (!sentry) {
+            return in;
+        }
+        char c = '0';
+        in >> c;
+        if (in && (c != dest.exp)) {
+            in.setstate(std::ios::failbit);
+        }
+        return in;
     }
-    return EXIT_SUCCESS;
+
+    std::istream& operator>>(std::istream& in, LIT&& dest) {
+        std::istream::sentry sentry(in);
+        if (!sentry) {
+            return in;
+        }
+        return in >> dest.ref;
+    }
+
+    std::istream& operator>>(std::istream& in, OCT&& dest) {
+        std::istream::sentry sentry(in);
+        if (!sentry) {
+            return in;
+        }
+        return in >> dest.ref;
+    }
+
+    std::istream& operator>>(std::istream& in, StringIO&& dest) {
+        std::istream::sentry sentry(in);
+        if (!sentry) {
+            return in;
+        }
+        return std::getline(in >> DelimiterIO{ '"' }, dest.ref, '"');
+    }
+
+    std::istream& operator>>(std::istream& in, DataStruct& dest) {
+        std::istream::sentry sentry(in);
+        if (!sentry) {
+            return in;
+        }
+
+        DataStruct input;
+        {
+            using sep = DelimiterIO;
+            using lit = LIT;
+            using oct = OCT;
+            using str = StringIO;
+
+            in >> sep{ '(' };
+            bool flag1 = false;
+            bool flag2 = false;
+            bool flag3 = false;
+            while (true) {
+                if (flag1 && flag2 && flag3) {
+                    break;
+                }
+                std::string key;
+                char c;
+                in >> c;
+                if (!in) {
+                    break;
+                }
+
+                if (c == ':' && (in >> key)) {
+                    if (key == "key1") {
+                        in >> lit{ input.key1 } >> sep{ 'u' } >> sep{ 'l' } >> sep{ 'l' };
+                        flag1 = true;
+                    }
+                    else if (key == "key2") {
+                        in >> sep{ '0' } >> sep{ 'b' } >> oct{ input.key2 };
+                        flag2 = true;
+                    }
+                    else if (key == "key3") {
+                        in >> str{ input.key3 };
+                        flag3 = true;
+                    }
+                }
+            }
+            in >> sep{ ':' } >> sep{ ')' };
+        }
+        if (in) {
+            dest = input;
+        }
+        return in;
+    }
+
+    std::ostream& operator<<(std::ostream& out, const DataStruct& src) {
+        std::ostream::sentry sentry(out);
+        if (!sentry) {
+            return out;
+        }
+        iofmtguard fmtguard(out);
+        out << "(";
+        out << ":key1 " << src.key1 << "ull";
+        out << ":key2 " << binaryNull(src.key2);
+        out << ":key3 " << "\"" << src.key3 << "\"";
+        out << ":)";
+        return out;
+    }
+
+    bool compareDataStruct(const DataStruct& a, const DataStruct& b) {
+        if (a.key1 != b.key1) {
+            return a.key1 < b.key1;
+        }
+        else if (a.key2 != b.key2) {
+            return a.key2 < b.key2;
+        }
+        else {
+            return a.key3.length() < b.key3.length();
+        }
+    }
+
+    iofmtguard::iofmtguard(std::basic_ios<char>& stream) :
+        stream_(stream),
+        fill_(stream.fill()),
+        precision_(stream.precision()),
+        flags_(stream.flags()) {}
+
+    iofmtguard::~iofmtguard() {
+        stream_.fill(fill_);
+        stream_.precision(precision_);
+        stream_.flags(flags_);
+    }
 }
--- umidov.ali/T2/item_data.h
+++ umidov.ali/T2/item_data.h
@@ -1,5 +1,6 @@
 #ifndef ITEM_DATA_H
 #define ITEM_DATA_H
+
 #include <iostream>
 #include <sstream>
 #include <vector>
@@ -8,51 +9,56 @@
 #include <algorithm>
 #include <bitset>
 #include <string>
-namespace umidov
-{
-    struct DataStruct
-    {
+
+namespace umidov {
+    struct DataStruct {
         unsigned long long key1;
         unsigned long long key2;
         std::string key3;
     };
-    struct DelimiterIO
-    {
+
+    struct DelimiterIO {
         char exp;
     };
-    struct StringIO
-    {
+
+    struct StringIO {
         std::string& ref;
     };
-    struct LIT
-    {
+
+    struct LIT {
         unsigned long long& ref;
     };
-    struct OCT
-    {
+
+    struct OCT {
         unsigned long long& ref;
     };
-    class iofmtguard
-    {
+
+    class iofmtguard {
     public:
         iofmtguard(std::basic_ios<char>& stream);
+
         ~iofmtguard();
+
     private:
         std::basic_ios<char>& stream_;
         char fill_;
         std::streamsize precision_;
-        std::basic_ios<char>::fmtflags flags_;
+        std::ios_base::fmtflags flags_;
     };
-    std::istream& readDelimiter(std::istream& input, DelimiterIO&& destination);
-    std::istream& readString(std::istream& input, StringIO&& destination);
-    std::istream& readULongLiteral(std::istream& input, LIT&& destination);
-    std::istream& readULongBinaryLiteral(std::istream& input, OCT&& destination);
+
     std::istream& operator>>(std::istream& input, DelimiterIO&& destination);
-    std::istream& operator>>(std::istream& input, StringIO&& destination);
+
     std::istream& operator>>(std::istream& input, LIT&& destination);
+
     std::istream& operator>>(std::istream& input, OCT&& destination);
+
+    std::istream& operator>>(std::istream& input, StringIO&& destination);
+
     std::istream& operator>>(std::istream& input, DataStruct& destination);
+
     std::ostream& operator<<(std::ostream& output, const DataStruct& source);
+
     bool compareDataStruct(const DataStruct& first, const DataStruct& second);
 }
+
 #endif
