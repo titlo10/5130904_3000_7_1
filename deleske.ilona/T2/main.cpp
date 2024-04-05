cfcb1040412f4f5614ea4d6a40d5d53789a5aef5
IlonaDeleske <ilona.deleske@yandex.ru>
2024-04-05 23:15:14 +0300

    Initial commit for T2

--- deleske.ilona/T2/main.cpp
+++ deleske.ilona/T2/main.cpp
@@ -11,15 +11,10 @@ int main() {
   vector<DataStruct> ds_original;
   string request;
   while (getline(cin, request)) {
-    if (request.empty()) {
-        break;
-    }
     istringstream iss(request);
     DataStruct temp;
     if (iss >> temp) {
       ds_original.push_back(temp);
-    } else {
-      break;
     }
   }
 
@@ -36,4 +31,3 @@ int main() {
   copy(ds_original.begin(), ds_original.end(), ostream_iterator<DataStruct>(cout, "\n"));
   return 0;
 }
-
