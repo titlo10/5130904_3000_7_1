8f2e922b5f53cd008f63292bb034199a5667cc0b
Sleewl <vyachaslav.gruzdev@yandex.ru>
2024-04-05 22:45:38 +0300

    next try

--- gruzdev.vyachaslav/T2/main.cpp
+++ gruzdev.vyachaslav/T2/main.cpp
@@ -1,57 +1,31 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <sstream>
-#include <cctype>
-struct DataStruct {
-    unsigned long long key1;
-    char key2;
-    std::string key3;
-};
-std::istream& operator>>(std::istream& in, DataStruct& data) {
-    in >> data.key1;
-    in >> data.key2;
-    std::getline(in >> std::ws, data.key3);
-    return in;
-}
-std::ostream& operator<<(std::ostream& out, const DataStruct& data) {
-    out << data.key1 << " " << data.key2 << " " << data.key3;
-    return out;
-}
-bool compareDataStruct(const DataStruct& a, const DataStruct& b) {
-    if (a.key1 != b.key1) {
-        return a.key1 < b.key1;
-    }
-    else if (a.key2 != b.key2) {
-        return a.key2 < b.key2;
-    }
-    else {
-        return a.key3.size() < b.key3.size();
-    }
-}
+#include "Iterator.h"
+
 int main() {
-    setlocale(LC_ALL, "Ru");
-    std::vector<DataStruct> dataVector;
-    DataStruct data;
-    std::cout << "Введите данные (формат: key1 key2 key3) или end для завершения ввода:" << std::endl;
-    std::string line;
-    while (std::getline(std::cin, line)) {
-        if (line == "end") {
-            break;
+    try {
+        std::string input = "";
+        std::vector< brat::DataStruct > data;
+
+        while (std::getline(std::cin, input)) {
+            std::istringstream iss(input);
+            brat::DataStruct temp;
+            if (iss >> temp) {
+                data.push_back(temp);
+            }
         }
-        std::istringstream iss(line);
-        if (iss >> data) {
-            dataVector.push_back(data);
+
+        std::sort(std::begin(data), std::end(data), brat::compareDataStruct);
+
+        for (const auto& d : data) {
+            std::cout << d << std::endl;
         }
     }
-    std::cout << "Данные перед сортировкой:" << std::endl;
-    for (const auto& data : dataVector) {
-        std::cout << data << std::endl;
+    catch (std::exception& er) {
+        std::cerr << er.what() << std::endl;
+        return EXIT_FAILURE;
     }
-    std::sort(dataVector.begin(), dataVector.end(), compareDataStruct);
-    std::cout << "Отсортированные данные:" << std::endl;
-    for (const auto& data : dataVector) {
-        std::cout << data << std::endl;
-    }
-    return 0;
+
+    return EXIT_SUCCESS;
 }
