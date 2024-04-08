dbc4bc831054ce65a4039cbd5c665299e766fefd
Luiza <kolpakovaluiza1@gmail.com>
2024-04-08 19:39:14 +0300

    Add new files

--- kolpakova.luiza/T2/main.cpp
+++ kolpakova.luiza/T2/main.cpp
@@ -1,2 +1,43 @@
+#include "namespace.h"
+
+using kolpakova::DataStruct;
+
 int main()
-{}
+{
+  try
+  {
+    setlocale(LC_ALL, "rus");
+    std::cout << "Введите строки в виде (:key1 value1(ull):key2 (0x)value2:key3 velue3:). value3 записывать в кавычках.\n";
+    std::cout << "Конец строк вызывается комбинацией Ctrl+Z.\n\n";
+    std::string input = "";
+    std::vector< kolpakova::DataStruct > data;
+
+    while (std::getline(std::cin, input)) {
+      std::istringstream iss(input);
+      kolpakova::DataStruct tmp;
+      try {
+        if (iss >> tmp) {
+          data.push_back(tmp);
+        }
+        else {
+          std::cerr << "Ошибка: Неверный формат строки! Попробуйте снова: " << std::endl;
+        }
+      }
+      catch (const std::exception& ex) {
+        std::cerr << "Ошибка: " << ex.what() << std::endl;
+      }
+    }
+
+    std::cout << "\nОтсортированнная часть: \n\n";
+
+    std::sort(data.begin(), data.end(), kolpakova::compareDataStruct);
+
+    std::copy(data.begin(), data.end(), std::ostream_iterator< kolpakova::DataStruct >(std::cout, "\n"));
+  }
+  catch (const std::exception& ex) {
+    std::cerr << ex.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
---
+++ kolpakova.luiza/T2/namespace.cpp
@@ -0,0 +1,197 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+namespace kolpakova
+{
+  struct DataStruct
+  {
+    unsigned long long key1;
+    unsigned long long key2;
+    std::string key3;
+  };
+
+  struct DelimiterIO
+  {
+    char exp;
+  };
+
+  struct ULongLiteralIO
+  {
+    unsigned long long& ref;
+  };
+
+  struct ULongHexLiteralIO
+  {
+    unsigned long long& ref;
+  };
+
+  struct StringIO
+  {
+    std::string& ref;
+  };
+
+  struct iofmtguard
+  {
+    std::basic_ios<char>& s_;
+    std::streamsize precision_;
+    std::ios::fmtflags fmt_;
+    char fill_;
+
+    iofmtguard(std::basic_ios<char>& s);
+    ~iofmtguard();
+  };
+
+  std::istream& operator>>(std::istream& in, DelimiterIO&& dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+    char c = '0';
+    in >> c;
+    if (in && (c != dest.exp))
+    {
+      in.setstate(std::ios::failbit);
+    }
+    return in;
+  }
+
+  std::istream& operator>>(std::istream& in, ULongLiteralIO&& dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+    return in >> dest.ref;
+  }
+
+  std::istream& operator>>(std::istream& in, ULongHexLiteralIO&& dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+    return in >> std::hex >> dest.ref;
+  }
+
+  std::istream& operator>>(std::istream& in, StringIO&& dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+    return std::getline(in >> DelimiterIO{ '"' }, dest.ref, '"');
+  }
+
+  std::istream& operator>>(std::istream& in, DataStruct& dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+
+    DataStruct input;
+    {
+      using sep = DelimiterIO;
+      using ull = ULongLiteralIO;
+      using ull1 = ULongHexLiteralIO;
+      using str = StringIO;
+
+      in >> sep{ '(' };
+      bool flag1 = false;
+      bool flag2 = false;
+      bool flag3 = false;
+      while (true)
+      {
+        if (flag1 && flag2 && flag3)
+        {
+          break;
+        }
+        std::string key;
+        char c;
+        in >> c;
+        if (!in)
+        {
+          break;
+        }
+        if (c == ':' && (in >> key))
+        {
+          if (key == "key1")
+          {
+            in >> ull{ input.key1 } >> sep{ 'u' } >> sep{ 'l' } >> sep{ 'l' };
+            flag1 = true;
+          }
+          else if (key == "key2")
+          {
+            in >> sep{ '0' } >> sep{ 'x' } >> ull1{ input.key2 };
+            flag2 = true;
+          }
+          else if (key == "key3")
+          {
+            in >> str{ input.key3 };
+            flag3 = true;
+          }
+        }
+      }
+      in >> sep{ ':' } >> sep{ ')' };
+    }
+    if (in)
+    {
+      dest = input;
+    }
+    return in;
+  }
+
+  std::ostream& operator<<(std::ostream& out, const DataStruct& src)
+  {
+    std::ostream::sentry sentry(out);
+    if (!sentry)
+    {
+      return out;
+    }
+    iofmtguard fmtguard(out);
+    out << "(";
+    out << ":key1 " << src.key1 << "ull";
+    std::ios::fmtflags prev_flags = out.flags();
+    out << ":key2 0x" << std::hex << src.key2;
+    out.flags(prev_flags);
+    out << ":key3 " << "\"" << src.key3 << "\"";
+    out << ":)";
+    return out;
+  }
+
+  bool compareDataStruct(const DataStruct& a, const DataStruct& b)
+  {
+    if (a.key1 != b.key1)
+    {
+      return a.key1 < b.key1;
+    }
+    else if (a.key2 != b.key2)
+    {
+      return a.key2 < b.key2;
+    }
+    else
+    {
+      return a.key3.length() < b.key3.length();
+    }
+  }
+
+  iofmtguard::iofmtguard(std::basic_ios<char>& s) :
+    s_(s),
+    fill_(s.fill()),
+    precision_(s.precision()),
+    fmt_(s.flags())
+  {}
+  iofmtguard::~iofmtguard()
+  {
+    s_.fill(fill_);
+    s_.precision(precision_);
+    s_.flags(fmt_);
+  }
+}
