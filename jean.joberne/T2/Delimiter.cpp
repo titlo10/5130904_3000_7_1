4efe1b9d57381b1c606fef783073590b869aa916
Jorbyart24 <jjorby00@gmail.com>
2024-04-10 03:51:20 +0300

    T2

---
+++ jean.joberne/T2/Delimiter.cpp
@@ -0,0 +1,31 @@
+#include "Delimiter.h"
+
+std::istream& jean::operator>>(std::istream& in, const Delimiter&& exp)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+  char c;
+  in >> c;
+  if (c != exp.expected)
+  {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+
+std::istream& jean::operator>>(std::istream& in, const StringDelimiter&& exp)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+  for (size_t i = 0; exp.expected[i] != '\0'; i++) {
+    in >> Delimiter{exp.expected[i]};
+  }
+  return in;
+}
+
