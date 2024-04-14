e914401925b3df80c23ee23a73f33bde562a97a0
Jorbyart24 <jjorby00@gmail.com>
2024-04-14 17:33:35 +0300

    jean.joberne/T2

--- jean.joberne/T2/DataStruct.cpp
+++ jean.joberne/T2/DataStruct.cpp
@@ -8,27 +8,39 @@ std::istream& jean::operator>>(std::istream& in, DataStruct& value)
     return in;
   }
 
-  char delimiter;
+  char delimiter, quote, colon;
+  long long numerator;
+  unsigned long long denominator;
+
   in >> delimiter;
   if (delimiter != '(') {
     in.setstate(std::ios::failbit);
     return in;
   }
 
+  in >> value.characterKey >> colon;
+  in >> quote;
+  if (quote != '\'') {
+    in.setstate(std::ios::failbit);
+    return in;
+  }
+  in >> numerator >> colon >> denominator;
+  if (colon != ':') {
+    in.setstate(std::ios::failbit);
+    return in;
+  }
+  value.rationalNumberKey = std::make_pair(numerator, denominator);
+  in >> quote;
+  in >> colon >> quote;
+  if (quote != '\"') {
+    in.setstate(std::ios::failbit);
+    return in;
+  }
+  std::getline(in, value.stringKey, '\"');
+  in >> delimiter;
+  if (delimiter != ')') {
+    in.setstate(std::ios::failbit);
+  }
   return in;
 }
 
-std::ostream& jean::operator<<(std::ostream& out, const DataStruct& value)
-{
-  std::ostream::sentry outputGuard(out);
-  if (!outputGuard)
-  {
-    return out;
-  }
-
-  out << "(:characterKey '" << value.characterKey;
-  out << "':rationalNumberKey (:N " << value.rationalNumberKey.first;
-  out << ":D " << value.rationalNumberKey.second << ":):stringKey ";
-  out << '"' << value.stringKey << '"' << ":)";
-  return out;
-}
