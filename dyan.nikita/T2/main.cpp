6cd56ab9d81c621a64ec34b1ca78be59867b5cb2
Дян Никита <moy-paa@mail.ru>
2024-04-22 16:55:05 +0300

    first commit

--- dyan.nikita/T2/main.cpp
+++ dyan.nikita/T2/main.cpp
@@ -1,2 +1,39 @@
+#include "Iterators.h"
+#include <algorithm>
+
+using dyan::DataStruct;
+
 int main()
-{}
+{
+  try
+  {
+    std::string input = "";
+    std::vector< DataStruct > data;
+
+    while (std::getline(std::cin, input))
+    {
+      std::istringstream iss(input);
+      DataStruct temp;
+      if (iss >> temp)
+      {
+        data.push_back(temp);
+      }
+    }
+
+    std::sort(std::begin(data), std::end(data), dyan::compareDataStruct);
+
+    std::copy
+    (
+      std::begin(data),
+      std::end(data),
+      std::ostream_iterator< DataStruct >(std::cout, "\n")
+    );
+  }
+  catch (std::exception& er)
+  {
+    std::cerr << er.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
