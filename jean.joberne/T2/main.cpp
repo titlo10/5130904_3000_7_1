b5709d8b93daeed1be8b8f829cbb80bcb4c15e3e
Jorbyart24 <jjorby00@gmail.com>
2024-04-22 16:42:47 +0300

    T2

--- jean.joberne/T2/main.cpp
+++ jean.joberne/T2/main.cpp
@@ -5,13 +5,10 @@ int main() {
     try {
         std::vector<DataStruct> data;
 
-        // Read DataStructs from standard input
         std::copy(std::istream_iterator<DataStruct>(std::cin), std::istream_iterator<DataStruct>(), std::back_inserter(data));
 
-        // Sort the data
         std::sort(data.begin(), data.end(), compareDataStruct);
 
-        // Output sorted data
         std::copy(data.begin(), data.end(), std::ostream_iterator<DataStruct>(std::cout, "\n"));
     }
     catch (std::exception& ex) {
@@ -21,3 +18,4 @@ int main() {
 
     return EXIT_SUCCESS;
 }
+
